d0557d7064e8d6470003331c1467158ca4a15799
holand76 <thegreath76@gmail.com>
2021-12-17 08:29:33 -0300

    doubel quotes agora sao single quotes

--- html_py_cpp/html_cpp.py
+++ html_py_cpp/html_cpp.py
@@ -10,15 +10,17 @@ def open_file(filename):
 def conversion(list_with_lines):
     cpp_string = ""
     for index in list_with_lines:
-        cpp_string += index.replace("/t","")
-        print(cpp_string)
+        string_edit = index.replace("/t","")
+        string_edit = index.replace('"',"'")
+        cpp_string += string_edit
+        print(cpp_string,"DEBUG_______")
     return cpp_string
 
 name = input("Name of the file? ")
 list_lines = open_file(name)
 total_string = conversion(list_lines)
 
-
+        
 
 print(repr(total_string))
 
